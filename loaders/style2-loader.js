545b117aa76538a1ddd26c9ce4679ab192773ed0
那个人就是我。 <32756980+vayne1Q@users.noreply.github.com>
2021-03-02 14:23:19 +0800

    Update and rename sprite2-loader to style2-loader.js

---
+++ loaders/style2-loader.js
@@ -0,0 +1,25 @@
+const { getOptions, stringifyRequest } = require('loader-utils')
+
+function loader(source) {
+  let script = `
+    let style = document.createElement('style');
+    style.innerHTML = ${JSON.stringify(source)}
+    document.head.appendChild(style)
+  `
+  return script
+}
+
+loader.pitch = (remainingRequest, previousRequest, data) => {
+  console.log(stringifyRequest(this, '!!' + remainingRequest), remainingRequest)
+  let script = `
+    let style = document.createElement('style');
+    style.innerHTML = require(${stringifyRequest(
+      this,
+      '!!' + remainingRequest
+    )})
+    document.head.appendChild(style)
+  `
+  return script
+}
+
+module.exports = loader
